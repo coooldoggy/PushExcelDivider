cc36dac009ac07c7f03b47627698c57f0a5fcf12
yulimlee <yulimlee@atoncorp.com>
2021-01-14 09:44:37 +0900

    exe file 추가

--- ExcelProcesser.py
+++ ExcelProcesser.py
@@ -17,14 +17,14 @@ def main(argv):
     rowArray = df[:]
     pd.set_option('display.max_rows', 100000)
     now = time.strftime('%Y_%m_%d', time.localtime(time.time()))
-    path = f'{outPath}/exceloutput_{now}'
+    path = "{}/exceloutput_{}".format(outPath, now)
     if not os.path.exists(path):
         os.mkdir(path)
     n = 1000
     i = 0
     j = 1
     for i in range(0, len(rowArray), n):
-        df['CTN'][i:i+n].to_csv(f'{path}/output{j}.txt',
+        df['CTN'][i:i+n].to_csv("{}/output{}.txt".format(path, j),
                                 mode='w', index=False, header=False)
         i += 1000
         j += 1
